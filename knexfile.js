9a3f567d85881d3a438442b55b4cb204e662ca0d
Matthew Gordon <laxredemption5525@gmail.com>
2017-01-30 09:29:20 -0700

    Configure database connection, add migration, add seed for --env test/development

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,35 @@
 
 module.exports = {
 
-  development: { },
-  test: { },
-  production: { },
+  development: {
+    client: 'pg',
+    connection: 'postegres://localhost/classifieds',
+    migrations : {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
+  test: {
+    client: 'pg',
+    connection: 'postegres://localhost/classifieds_test',
+    migrations : {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/test'
+    }
+   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations : {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/production'
+    }
+   },
 
 };
